26acbb6fc9dfd186342e2b96dd43bb7b008e171a
Michael Gregory <michael.gregory8584@gmail.com>
2020-09-04 19:59:04 -0500

    MVP I didn't reach mvp on this prior. Styling was heavily borrowed from https://freefrontend.com/css-cards/ author: black ellis
    working on search bar now
    
    I forsee needing to create state for the form and a variable for the
    username that will be used in the axios call

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -8,31 +8,48 @@ import './App.css';
 
 class App extends React.Component {
   state = {
-     userdata: []
+     userdata: [],
+     followdata: []
   }
 
+  
+
   componentDidMount() {
+    console.log("componentDidMount")
+
     // fetch("https://api.github.com/users/MikeG353")
     //  .then(res => res.json())
     //  .then(console.log(json))
     axios.get("https://api.github.com/users/MikeG353")
       .then(res => {
+        console.log("axios res: ", res)
         this.setState({
           userdata: res.data
         })
+        console.log("state after axios call: ", this.state)
       })
       .catch(err => {
         console.log(err)
-      })
+    })
+
+    axios.get("https://api.github.com/users/MikeG353/followers")
+      .then(res => {
+        console.log("followers: ", res.data)
+        this.setState({
+          followdata: res.data
+        })
+        console.log("state after follower call: ", this.state)
+    })
   }
 
   render() {
-    console.log(this.state)
+    console.log("render runs")
     return (
       <div className="App">
         <div className="header">
-          <h1>Test</h1>
-          <UserCard userdata={this.state.userdata}/>
+          <h1>Github User Card</h1>
+          <h2>by: Michael Gregory</h2>
+          <UserCard userdata={this.state.userdata} followdata={this.state.followdata}/>
         </div>
       </div>
     )
--- github-user-card/src/Components/UserCard.js
+++ github-user-card/src/Components/UserCard.js
@@ -4,11 +4,30 @@ import React from "react"
 // maybe a follower counter nested inside the card
 // prop drill for that.
 
+// I'm not sure I fully understand the readme
+// the follower count is pulled in the initial axios request and can
+// be accessed already as shown
+
 function UserCard(props) {
-    console.log(props.userdata.login)
+    console.log("UserCard 'render':: props: ", props)
+
     return (
         <div className="user-card" props={props}>
-            <img src={props.userdata.avatar_url} alt="user-image" />
+            <img className="user-image" src={props.userdata.avatar_url} alt="user-image" />
+            <p className="user-name">Full Name: {props.userdata.name}</p>
+            <p>Github Username: {props.userdata.login}</p>
+            <p>{props.userdata.bio}</p>
+            <p>Repositories: {props.userdata.public_repos}</p>
+            <div className="follower-counts">
+                <p className="follow-data">Followers : {props.userdata.followers}</p>
+                <p className="follow-data">Following : {props.userdata.following}</p>
+            </div>
+            <div>
+                <h3>Followers:</h3>
+               {props.followdata.map((follower) => ( 
+                    <p key={follower.id}>{follower.login}</p>
+                ))}               
+            </div>
         </div>
     )
 }
