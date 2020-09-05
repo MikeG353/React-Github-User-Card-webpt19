139809c7e80aefd4f2617a541fddd66f8a49bd1d
Michael Gregory <michael.gregory8584@gmail.com>
2020-09-04 23:04:28 -0500

    search bar added moved axios requests to a function to clean up.

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -2,37 +2,40 @@ import React from 'react';
 
 //component imports
 import UserCard from './Components/UserCard'
+import SearchForm from './Components/SeachForm'
 
 import axios from 'axios'
 import './App.css';
 
 class App extends React.Component {
-  state = {
-     userdata: [],
-     followdata: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "MikeG353",
+      userdata: [],
+      followdata: []
+    }
   }
 
-  
-
-  componentDidMount() {
-    console.log("componentDidMount")
-
-    // fetch("https://api.github.com/users/MikeG353")
-    //  .then(res => res.json())
-    //  .then(console.log(json))
-    axios.get("https://api.github.com/users/MikeG353")
-      .then(res => {
-        console.log("axios res: ", res)
-        this.setState({
-          userdata: res.data
-        })
-        console.log("state after axios call: ", this.state)
-      })
-      .catch(err => {
-        console.log(err)
+  changeUsername = (username) => {
+    this.setState({
+      username: username
     })
+    console.log(this.state.username)
 
-    axios.get("https://api.github.com/users/MikeG353/followers")
+  }
+  getUserData = (username) => {
+    axios.get(`https://api.github.com/users/${username}`)
+    .then(res => {
+      this.setState({
+        userdata: res.data
+      })
+      console.log("state after axios call: ", this.state)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+    axios.get(`https://api.github.com/users/${username}/followers`)
       .then(res => {
         console.log("followers: ", res.data)
         this.setState({
@@ -40,7 +43,28 @@ class App extends React.Component {
         })
         console.log("state after follower call: ", this.state)
     })
+
   }
+  componentDidMount() {
+    console.log("componentDidMount")
+    // fetch("https://api.github.com/users/MikeG353")
+    //  .then(res => res.json())
+    //  .then(console.log(json))
+    this.getUserData(this.state.username)
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log(
+      "CDU runs: prevProps, prevState: ",
+      prevProps,
+      prevState,
+      this.state.username
+    );
+
+    if (prevState.username !== this.state.username) {
+      this.getUserData(this.state.username)
+    }
+  }
+  
 
   render() {
     console.log("render runs")
@@ -49,6 +73,8 @@ class App extends React.Component {
         <div className="header">
           <h1>Github User Card</h1>
           <h2>by: Michael Gregory</h2>
+          <SearchForm changeUsername={this.changeUsername} username={this.state.username}/>
+        
           <UserCard userdata={this.state.userdata} followdata={this.state.followdata}/>
         </div>
       </div>
--- github-user-card/src/Components/UserCard.js
+++ github-user-card/src/Components/UserCard.js
@@ -1,13 +1,5 @@
 import React from "react"
 
-// I'm not thinking the card itself needs state.
-// maybe a follower counter nested inside the card
-// prop drill for that.
-
-// I'm not sure I fully understand the readme
-// the follower count is pulled in the initial axios request and can
-// be accessed already as shown
-
 function UserCard(props) {
     console.log("UserCard 'render':: props: ", props)
 
@@ -16,13 +8,13 @@ function UserCard(props) {
             <img className="user-image" src={props.userdata.avatar_url} alt="user-image" />
             <p className="user-name">Full Name: {props.userdata.name}</p>
             <p>Github Username: {props.userdata.login}</p>
-            <p>{props.userdata.bio}</p>
+            <p className="user-bio">{props.userdata.bio}</p>
             <p>Repositories: {props.userdata.public_repos}</p>
             <div className="follower-counts">
                 <p className="follow-data">Followers : {props.userdata.followers}</p>
                 <p className="follow-data">Following : {props.userdata.following}</p>
             </div>
-            <div>
+            <div className="follower-list">
                 <h3>Followers:</h3>
                {props.followdata.map((follower) => ( 
                     <p key={follower.id}>{follower.login}</p>
